c0bf3790d7c098b6b83ae976044a6fc016707222
larswachter <lars97.waechter@gmail.com>
2025-08-24 12:49:34 +0200

    Tests

--- projects/markular/src/lib/markular.component.spec.ts
+++ projects/markular/src/lib/markular.component.spec.ts
@@ -1,16 +1,24 @@
+import 'zone.js';
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Markular } from './markular.component';
+import { CommonModule } from '@angular/common';
 
-describe('MarkdownEditor', () => {
+describe('markular', () => {
   let component: Markular;
   let fixture: ComponentFixture<Markular>;
 
+  /*
+  beforeAll(() => {
+    TestBed.initTestEnvironment(BrowserTestingModule, platformBrowserTesting());
+  });
+   */
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Markular]
-    })
-    .compileComponents();
+      imports: [Markular, CommonModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(Markular);
     component = fixture.componentInstance;
@@ -19,5 +27,11 @@ describe('MarkdownEditor', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should insert H1', () => {
+    component._val = 'Hello';
+    component.toggleHeading(1);
+    expect(component._val).toBe('# Hello');
   });
 });
